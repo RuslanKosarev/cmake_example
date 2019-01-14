f54be56fdd8db89aa471082c0742ed57dabe88ff
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2019-01-14 21:24:12 +0300

    update example

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,20 @@
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
-    return i + j;
-}
+class Pet {
+	public:
+		Pet(const std::string &name_){ name = name_;}
+		void setName(const std::string &name_) { name = name_; }
+		const std::string &getName() const { return name; }
 
-int multiply(int i, int j) {
-    return i * j;
-}
+	private:
+		std::string name;
+};
 
-int substract(int i, int j) {
-    return i - j;
-}
 namespace py = pybind11;
 
-PYBIND11_MODULE(cmake_example, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: cmake_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-	m.def("multiply", &multiply, R"pbdoc(
-        Multiply two numbers
-
-        Some other explanation about the multiply function.
-    )pbdoc");
-	
-    m.def("subtract", &substract, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
-#ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
-#else
-    m.attr("__version__") = "dev";
-#endif
+PYBIND11_MODULE(example, m) {
+    py::class_<Pet>(m, "Pet")
+        .def(py::init<const std::string &>())
+        .def("setName", &Pet::setName)
+        .def("getName", &Pet::getName);
 }
